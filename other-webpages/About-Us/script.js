f425ec7c07ab47cf991ea5fab36c96815c72fdf8
xdnexus <151912914+xdnexus@users.noreply.github.com>
2024-01-08 16:52:27 +0800

    Add files via upload

---
+++ other-webpages/About-Us/script.js
@@ -0,0 +1,6 @@
+// Get screen dimensions
+var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+// Show alert with screen dimensions
+alert("Screen Width: " + screenWidth + " pixels\nScreen Height: " + screenHeight + " pixels");
